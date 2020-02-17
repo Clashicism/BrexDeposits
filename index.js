94f07a0c61145410fad2fbcc17f46c91a162f4a6
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-02-16 20:37:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ let listen_slp = async function () {
         if(event.type == 'mempool' && event.data[0].valid) {
 //            console.log(event.data[0]);
   //          console.log(event.data[0].token);
-            console.log(tokens[event.data[0].amount] + " " + tokens[event.data[0].token]);
+            console.log(event.data[0].amount + " " + tokens[event.data[0].token]);
             
         }
 
