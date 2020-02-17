b1f4079e53c496f0f432ea26694466a4e5da0ef6
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-02-17 05:47:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ let listen_slp = async function () {
 //            console.log(event.data[0].amount + " " + tokens[event.data[0].token]);
             console.log(tokens[event.data[0].token]);
             console.log(decimals[tokens[event.data[0].token]]);
-            console.log(10^-(decimals[tokens[event.data[0].token]]));
-            amount=event.data[0].out[0].amount^-(decimals[tokens[event.data[0].token]]);
+            console.log(10**-(decimals[tokens[event.data[0].token]]));
+            amount=event.data[0].out[0].amount**-(decimals[tokens[event.data[0].token]]);
             console.log(amount);
             
         }
