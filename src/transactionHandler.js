476267ad9c27599fd201ccf9301da3e9bb28af27
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-02-26 13:23:18 -0500

    Update transactionHandler.js

--- src/transactionHandler.js
+++ src/transactionHandler.js
@@ -4,11 +4,11 @@ const { notification } = require("./notification/notification");
 const tokenType = process.env.TOKENTYPE;
 const tokenDecimalPlaces = process.env.DECIMALPLACES;
 
-var amount=0; var exp=0;
-const frog=0; const honk=1; 
+// var amount=0; var exp=0;
+// const frog=0; const honk=1; 
+var decimals = ["0","1"];
 var tokens = ["c8947a3c68dfa4c1c4f5112132b6518aff9b9aa42d823780f52b06c2faf7005e",
               "7f8889682d57369ed0e32336f8b7e0ffec625a35cca183f4e81fde4e71a538a1"];
-var decimals = ["0","1"];
 tokens["CyFrog"]="c8947a3c68dfa4c1c4f5112132b6518aff9b9aa42d823780f52b06c2faf7005e";
 tokens["c8947a3c68dfa4c1c4f5112132b6518aff9b9aa42d823780f52b06c2faf7005e"]="CyFrog";
     decimals["CyFrog"]=2;
