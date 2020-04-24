5761385683b8a8d0aad3c5304c2dfa8350120059
Clashicism <60586534+Clashicism@users.noreply.github.com>
2020-04-24 19:02:20 -0400

    Update transactionHandler.js

--- src/transactionHandler.js
+++ src/transactionHandler.js
@@ -54,7 +54,7 @@ module.exports.transactionHandler = async trx => {
   if (trx.type === "mempool") {
     const txData = trx.data[0];
 //    let txnamount=trx.data[0].out[0].amount;
-    let exp=10**-(decimals[tokens[trx.data[0].slp.detail]]);
+    let exp=10**-(decimals[tokens[trx.data[0].slp.detail.tokenIdHex]]);
     let txnamount=trx.data[0].out[0].amount*exp;
     console.log(tokens[trx.data[0].slp.detail.tokenIdHex]);
     console.log(decimals[tokens[trx.data[0].slp.detail.tokenIdHex]]);    
